0e45c2f27c6dcf445bde4b5ff30b120edb2e8a7e
takuta9622 <t.takuya9622@gmail.com>
2025-01-04 15:17:51 +0900

    add header view

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>COACHTECH 勤怠管理</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+    @yield('css')
+</head>
+
+<body>
+    <header class="header">
+        <div class="header-container">
+            <div class="header-logo">
+                <a href="">
+                    <img src="{{ asset('images/logo.svg') }}" alt="COACHTECH">
+                </a>
+            </div>
+
+            @auth
+            <div class="header-nav">
+                <ul class="menu-items">
+                    @if (auth()->user()->isAdmin())
+                    <li><a href="">勤怠一覧</a></li>
+                    <li><a href="">スタッフ一覧</a></li>
+                    <li><a href="">申請一覧"></a></li>
+                    @else
+                    <li><a href="">勤怠</a></li>
+                    <li><a href="">勤怠一覧</a></li>
+                    <li><a href="">申請</a></li>
+                    @endif
+                    <li>
+                        <form action="{{ route('logout') }}" method="POST" class="logout-form">
+                            @csrf
+                            <button type="submit" class="header-nav-link">ログアウト</button>
+                        </form>
+                    </li>
+                </ul>
+            </div>
+            @endauth
+        </div>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+</body>
+
+</html>
