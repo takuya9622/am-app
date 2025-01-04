6639be9433a11f4f0d6e2fd0deea55b35b4def23
takuta9622 <t.takuya9622@gmail.com>
2025-01-04 16:34:20 +0900

    add function to register user

---
+++ src/app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required','string', 'email', 'max:255', 'unique:users,email'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password_confirmation' => ['required', 'string', 'min:8', 'same:password'],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'お名前を入力してください',
+            'name.string' => 'お名前は文字列で入力してください',
+            'name.max' => 'お名前は255文字以内で入力してください',
+            'email.required' => 'メールアドレスを入力してください',
+            'email.string' => 'メールアドレスは文字列で入力してください',
+            'email.email' => '有効なメールアドレスを入力してください',
+            'email.max' => 'メールアドレスは255文字以内で入力してください',
+            'email.unique' => 'このメールアドレスは登録されています',
+            'password.required' => 'パスワードを入力してください',
+            'password.string' => 'パスワードは文字列で入力してください',
+            'password.min' => 'パスワードは8文字以上で入力してください',
+            'password.confirmed' => '確認用パスワードと一致しません',
+            'password_confirmation.required' => '確認用パスワードを入力して下さい',
+            'password_confirmation.string' => '確認用パスワードは文字列で入力してください',
+            'password_confirmation.min' => '確認用パスワードは8文字以上で入力してください',
+            'password_confirmation.same' => 'パスワードと一致しません',
+        ];
+    }
+}
