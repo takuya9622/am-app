ea895c70cc7d128499772100ee06c04472608d96
takuta9622 <t.takuya9622@gmail.com>
2025-01-27 20:49:03 +0900

    add function to login as admin

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,14 @@
 
 use App\Http\Controllers\AttendanceController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
+
+Route::middleware('guest')->group(function () {
+    Route::get('admin/login', [AuthenticatedSessionController::class, 'create'])->name('admin.login');
+    Route::post('admin/login', [AuthenticatedSessionController::class, 'store']);
+});
+
+
 Route::middleware(['auth', 'staff'])->group(function () {
     Route::resource('attendance', AttendanceController::class)
         ->only(['index', 'store', 'update']);
